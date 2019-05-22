e30bd41c53ea138286c1ceb1e51376d9e02322d5
duckets615 <chris.reilly024@gmail.com>
2019-05-22 18:42:32 -0400

    Peak index of Mountain

---
+++ Peak Index of Mountain.js
@@ -0,0 +1,13 @@
+//const peakIndexInMountainArray = A => A.indexOf(Math.max(...A));
+
+// function peakIndexInMountainArray(A) {
+//     for (let i = 0; i < A.length; i++) {
+//         if (A[i] > A[i-1] && A[i] > A[i+1]) return i;
+//     }
+//     `A is ot a mountain or has no single highest peak`
+// }
+    
+
+
+// console.log(peakIndexInMountainArray([0,1,0]));
+// console.log(peakIndexInMountainArray([0,2,1,0]));
