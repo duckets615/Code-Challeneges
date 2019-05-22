b4dcfa07b9b284c2dd3c4bc8997a1bc5d31ace51
duckets615 <chris.reilly024@gmail.com>
2019-05-22 18:53:36 -0400

    162. Array Parity

--- Array Parity.js
+++ Array Parity.js
@@ -1,11 +1,19 @@
-// function sortArrayByParity(A) {
-//     return A.filter(ele => ele % 2 === 0).concat(A.filter(ele => ele % 2 !== 0))
-// };
+//    162. Find Peak Element
+
+/* A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞. */
 
 
-//console.log(sortArrayByParity([4,2,3,1], [2,4,1,3]))
 
 
-// const sortArrayByParity = A => A.filter(ele => ele % 2  === 0).concat(A.filter(ele => ele % 2 !== 0));
 
+
+const sortArrayByParity = A => A.filter(ele => ele % 2  === 0).concat(A.filter(ele => ele % 2 !== 0));
+
+console.log(sortArrayByParity([4,2,3,1], [2,4,1,3]))
 
