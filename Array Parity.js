2f6412f5e37530e8b41c2cba6e2505c58f787ec4
duckets615 <chris.reilly024@gmail.com>
2019-05-22 18:42:17 -0400

    Array Parity

--- Array Parity.js
+++ Array Parity.js
@@ -3,9 +3,9 @@
 // };
 
 
-const sortArrayByParity = A => A.filter(ele => ele % 2  === 0).concat(A.filter(ele => ele % 2 !== 0));
+//console.log(sortArrayByParity([4,2,3,1], [2,4,1,3]))
 
 
+// const sortArrayByParity = A => A.filter(ele => ele % 2  === 0).concat(A.filter(ele => ele % 2 !== 0));
 
-console.log(sortArrayByParity([4,2,3,1], [2,4,1,3]))
- 
+
