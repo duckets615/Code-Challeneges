60fdb1b62045633e09e1390abc4ab434d4215054
duckets615 <chris.reilly024@gmail.com>
2019-05-23 20:30:21 -0400

    557 reverse words

---
+++ 557 reverse words in a string III.js
@@ -0,0 +1,12 @@
+//  557 Reverse words in a string III
+
+
+//Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+
+
+const reverseWords = s => s.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+
+
+console.log(reverseWords("Let's take LeetCode Contest"))
+//                                                       "s'teL ekat edoCteeL tsetnoc"
