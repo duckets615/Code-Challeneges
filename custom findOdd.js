968534584fd2b3f037116cb1f87c0e4a6f21b7cb
duckets615 <chris.reilly024@gmail.com>
2019-05-28 21:50:02 -0400

    added obj solution

--- custom findOdd.js
+++ custom findOdd.js
@@ -12,21 +12,28 @@ There will always be one value that repeats an odd number of times;
 
 
 
+// function findOdd(array) {
+//     let count = 0, length = array.length;
+//     for (let i = 0; i < length; i++) {
+//         count = 0;
+//         for (let j = 0; j < length; j++) {
+//             if (array[i] === array[j]) count++;
+//         }
+//         if (count % 2 !== 0) {
+//             return array[i]
+//         } else {
+//             count = 0;
+//         }
+//     }
+// }
+
 function findOdd(array) {
-    let count = 0, length = array.length;
-    for (let i = 0; i < length; i++) {
-        count = 0;
-        for (let j = 0; j < length; j++) {
-            if (array[i] === array[j]) count++;
-        }
-        if (count % 2 !== 0) {
-            return array[i]
-        } else {
-            count = 0;
-        }
+    let obj = {};
+    array.map(ele => obj[ele] === undefined ? obj[ele] = 1 : obj[ele] += 1)
+    for (var ele in obj) {
+        if (obj[ele] % 2 !== 0) return ele
     }
 }
-
 
 console.log(findOdd([1,1,2,2,2,3,3])) // 2
 console.log(findOdd([6,6,7,7,8,8,8])) // 8
