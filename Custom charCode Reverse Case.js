7b6312e3de628cb9fad017dbf701e7cacb112a10
duckets615 <chris.reilly024@gmail.com>
2019-06-02 14:35:46 -0400

    Custom charCode Reverse Case

---
+++ Custom charCode Reverse Case.js
@@ -0,0 +1,22 @@
+// Without using either the toLowerCase or toUppercase method, reverse the casing
+// for each letter in a given string.
+// ex. 'abcd' => 'ABCD',  'xYZ' => 'Xyz'
+
+//  hint:   ACSII = 'American Standard Code for Information Interchange'
+
+
+
+function reverseCase(str) {
+    str.split('').map(ele => {
+        let code = str.charCodeAt(ele);
+        code < 97 ? ele = String.fromCharCode(code-32) : ele = String.fromCharCode(code+32);
+    });
+    return str;
+}
+
+
+
+console.log(reverseCase('abcd')) // 'ABCD';
+console.log(reverseCase('xYZ')) // 'Xyz';
+console.log(reverseCase('uPPerCase')) // 'UppERcASE';
+console.log(reverseCase('LOWerCASe')) // 'lowERcasE';
