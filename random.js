04ac6648e002bf42152602363e319a40df35b1d9
duckets615 <chris.reilly024@gmail.com>
2019-07-06 22:06:03 -0400

    update

--- random.js
+++ random.js
@@ -151,20 +151,154 @@
 
 // Have the function NumberStream(str) take the str parameter being passed which will contain the numbers 2 through 9, and determine if there is a consecutive stream of digits of at least N length where N is the actual digit value. If so, return the string true, otherwise return the string false. For example: if str is "6539923335" then your program should return the string true because there is a consecutive stream of 3's of length 3. The input string will always contain at least one digit.
 
-function NumberStream(str) { 
-    for (let i = 0; i < str.length; i++) { 
-        if (str[i] === str[i-1]) continue; // cut down on # of operations, no point in evaluating non essential nums; 
-        
-        let start = i;  // start idx for slice method
-        let end = i + +str[i]; // ending idx for slice method
-        let target = str[i] ** 2; // an array of #'s repeated its value # of times is the same as num squared;
+// function NumberStream(str) { 
+//     for (let i = 0; i < str.length; i++) { 
+//         if (str[i] === str[i-1]) continue;
+//         let start = i;
+//         let end = i + +str[i];
+//         let target = str[i] ** 2;
+//         let seq = str.slice(start, end).split('').map(num => +num).reduce((acc,cur) => acc + cur);
+//         if (seq === target) return true;
+//         seq = [];
+//     }
+//     return false;
+// }
+// NumberStream("5556293383563665")/*?*/ //false
+// NumberStream("5788888888882339999") /*?*/ // true
 
-        let seq = str.slice(start, end).split('').map(num => +num).reduce((acc,cur) => acc + cur); 
-        seq
-        if (seq === target) return true;
-        seq = [];
+
+
+// 1. It needs to contain three sets each with three digits (1 through 9) separated by a period.
+// 2. The first set of digits must add up to an even number.
+// 3. The second set of digits must add up to an odd number.
+// 4. The last digit in each set must be larger than the two previous digits in the same set.
+
+// If all the above constraints are met within the string, the your program should return the string true, otherwise your program should return the string false. For example: if str is "224.315.218" then your program should return "true".
+
+// function SerialNumber(str) {
+//     let ar = str.split('.');
+//     for (let i = 0; i < ar.length; i++) {
+//         if (ar[i].length < 3) return false;
+//         if (ar[i][2] < ar[i][1] || ar[i][2] < ar[i][0]) return false;
+//         if (ar[0].split('').map(num => +num).reduce((acc,cur) => acc + cur) % 2 !== 0) return false;
+//         if (ar[1].split('').map(num => +num).reduce((acc,cur) => acc + cur) % 2 === 0) return false;
+//     }
+//     return true;
+//   }
+
+// SerialNumber("11.124.667") /*?*/ // false;
+// SerialNumber("114.568.112") /*?*/ // true;
+
+
+
+// Have the function ThreeNumbers(str) take the str parameter being passed and determine if exactly three unique, single-digit integers occur within each word in the string. The integers can appear anywhere in the word, but they cannot be all adjacent to each other. If every word contains exactly 3 unique integers somewhere within it, then return the string true, otherwise return the string false. For example: if str is "2hell6o3 wor6l7d2" then your program should return "true" but if the string is "hell268o w6or2l4d" then your program should return "false" because all the integers are adjacent to each other in the first word.
+// Sample Test Cases
+
+// function ThreeNumbers(str) { 
+//     let ar = str.split(' '), Length = ar.length, count = 0;
+//     for (let i = 0; i < Length; i++) {
+//         if (count === 3) return true;
+//         count = 0;
+//         for (let j = 0; j < ar[i].length; j++) {
+//             let cur = ar[i][j];
+//             let next = ar[i][j+1];
+//             let prev = ar[i][j-1];
+
+//             if (cur.match(/[0-9]/)) {
+//                 if (prev === undefined || !prev.match(/[0-9]/)) {
+//                     if (next === undefined || !next.match(/[0-9]/)) count++;
+//                 } else {
+//                     continue;
+//                 }
+//             } else {
+//                 continue;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+
+// ThreeNumbers("2a3b5 w1o2rl3d g1gg92") /*?*/ // true
+// ThreeNumbers("21aa3a ggg4g4g6ggg") /*?*/ // false
+
+// function canAlternate(s) {
+// 	let one = s.split('').filter(num => num === 0).length;
+// 	let two = s.split('').filter(num => num === 1).length;
+// 	return Math.abs(one - two === 1)
+// }
+// const canAlternate = s => Math.abs(s.split('').filter(num => +num === 0).length - s.split('').filter(num => +num === 1).length) < 2;
+
+// canAlternate("0001111") /*?*/ // true)
+// canAlternate("01001") /*?*/ // true)
+// canAlternate("010001") /*?*/ // false)
+// canAlternate("0100110111") /*?*/ // false)
+// canAlternate("10101010") /*?*/ // true)
+// canAlternate("010101000") /*?*/ // false)
+// canAlternate("0111") /*?*/ // false)
+// canAlternate("00") /*?*/ // false)
+// canAlternate("1111") /*?*/ // false)
+// canAlternate("101") /*?*/ // true)
+
+// Have the function SumMultiplier(arr) take the array of numbers stored in arr and return the string true if any two numbers can be multiplied so that the answer is greater than double the sum of all the elements in the array. If not, return the string false. For example: if arr is [2, 5, 6, -6, 16, 2, 3, 6, 5, 3] then the sum of all these elements is 42 and doubling it is 84. There are two elements in the array, 16 * 6 = 96 and 96 is greater than 84, so your program should return the string true.
+
+// function SumMultiplier1(arr) { 
+//         let target = arr.reduce((acc,cur) => acc + cur) * 2;
+//         for (let i = 0; i < arr.length; i++) {
+//                 for (let j = i + 1; j < arr.length; j++) {
+//                         if (arr[i] * arr[j] > target) return true;
+//                     }
+//                 }
+//                 return false;           
+//             }
+
+// function SumMultiplier(arr) { 
+//     let sorted = arr.sort((a,b) => b - a);
+//     return sorted[0] * sorted[1] > arr.reduce((acc,cur) => acc + cur) * 2;
+// }
+// SumMultiplier([2, 2, 2, 2, 4, 1]) /*?*/ // false
+// SumMultiplier([1, 1, 2, 10, 3, 1, 12]) /*?*/ // true
+// SumMultiplier([4, 5, 5, 5, 12]) /*?*/ // false.
+
+
+// function isPrime(num) {
+//     if (num < 2) return false;
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
+
+// function PrimeMover(num) {
+//     let primes = [];
+//     for (let i = 2; primes.length < num; i++) {
+//         if (isPrime(i)) primes.push(i)
+//     }
+//     return primes[num-1];
+// }
+
+// PrimeMover(9) /*?*/ // 23
+// PrimeMover(100) /*?*/ // 541
+// PrimeMover(1) /*?*/ // 2
+// PrimeMover(2) /*?*/ // 3
+// PrimeMover(3) /*?*/ // 5
+// PrimeMover(100) /*?*/ // 541
+// PrimeMover(400) /*?*/ // 2741
+// PrimeMover(565) /*?*/ // 4099
+// PrimeMover(312) /*?*/ // 2069
+// PrimeMover(567) /*?*/ // 4127
+// PrimeMover(90) /*?*/ // 463
+// PrimeMover(16) /*?*/ //53
+
+function DashInsertII(num) { 
+    let ar = num.toString().split('');
+    ar 
+    for (let i = 0; ar.length; i++) {
+        if (+ar[i] % 2 === 0 && +ar[i+1] % 2 === 0) ar.splice(i,0,'*');
+        if (+ar[i] % 2 !== 0 && +ar[i+1] % 2 !== 0) ar.splice(i,0,'-');
     }
-    return false;
+    return ar
 }
-//NumberStream("5556293383563665")/*?*/ //false
-NumberStream("5788888888882339999") /*?*/ // true
+
+DashInsertII(99946) /*?*/ // 9-9-94*6
+DashInsertII(56647304) /*?*/ // 56*6*47-304
